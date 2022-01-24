adff42c673362de855366b14183ae56baa70be99
Simon <simonbesteiro@gmail.com>
2022-01-24 15:10:04 +0100

    green

--- src/main/java/kata/LeapYear.java
+++ src/main/java/kata/LeapYear.java
@@ -2,7 +2,7 @@ package kata;
 
 public class LeapYear {
   public boolean isLeapYear(int year) {
-      if (year == 4)
+      if (year == 4 || year == 400)
           return true;
       return false;
   }
