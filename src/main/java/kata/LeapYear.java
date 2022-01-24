8c89d0868df3e5dfd79970d2b3537ed7056eb1f8
Simon <simonbesteiro@gmail.com>
2022-01-24 15:32:21 +0100

    refactor

--- src/main/java/kata/LeapYear.java
+++ src/main/java/kata/LeapYear.java
@@ -2,7 +2,9 @@ package kata;
 
 public class LeapYear {
   public boolean isLeapYear(int year) {
-      if(year == 1800 || year == 500 || year == 1000)
+      if(year % 4 == 0
+          && year % 100 == 0
+          && year % 400 != 0 )
           return false;
       if (year % 4 == 0)
           return true;
