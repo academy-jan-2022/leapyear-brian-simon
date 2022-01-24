aff786ef32c3af1134b3024d32ff1fab208735ea
Andra <andra@wdc-creative.com>
2022-01-24 14:27:36 +0000

    red

--- src/main/java/kata/LeapYear.java
+++ src/main/java/kata/LeapYear.java
@@ -2,7 +2,7 @@ package kata;
 
 public class LeapYear {
   public boolean isLeapYear(int year) {
-      if(year == 1800)
+      if(year == 1800 || year == 500)
           return false;
       if (year % 4 == 0)
           return true;
